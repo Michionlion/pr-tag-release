b75a7579888e7abe21a966a355821ec1cd7dfbea
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-04-05 19:46:28 -0400

    Add debug prints

--- pr_tag_release.sh
+++ pr_tag_release.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 LATEST_COMMIT_MSG="$(git log -1 --pretty=%B)"
+LATEST_VERSION="$(git describe --abbrev=0 --tags)"
+echo "READ: $LATEST_VERSION"
+LATEST_VERSION="$(expr "$LATEST_VERSION" : "v\(\d+\.\d+\.\d+\)")"
+echo "FIXED: $LATEST_VERSION"
+VERSION_SPLIT=(${LATEST_VERSION//./ })
+
+echo "ARRAY: $VERSION_SPLIT"
+
+#get number parts and increase last one by 1
+MAJOR=${VERSION_BITS[0]}
+MINOR=${VERSION_BITS[1]}
+PATCH=${VERSION_BITS[2]}
+
+echo "FINAL: $MAJOR $MINOR $PATCH"
 
 printf "MSG: %s" $LATEST_COMMIT_MSG
 
