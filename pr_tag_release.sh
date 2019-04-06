f987d08b1941509e808121dc77f23a1a789f2ab3
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-04-06 00:11:09 -0400

    Add first complete iteration

--- pr_tag_release.sh
+++ pr_tag_release.sh
@@ -1,29 +1,81 @@
 #!/bin/bash
 
+# This script requires Travis CI's env vars
+# and a GITHUB_OAUTH_TOKEN env var
+
+DEPLOY_BRANCH=${DEPOY_BRANCH-"^master$"}
+
+VERSION_CHANGE_PREFIX="This PR is a "
+VERSION_CHANGE_TYPES=("small (change|update)" "feature" "compatibility[ -]breaking (change|update)" "(non-|un)versioned (change|update)")
+
 LATEST_COMMIT_MSG="$(git log -1 --pretty=%B)"
 LATEST_VERSION=$(git describe --abbrev=0 --tags)
 LATEST_VERSION="${LATEST_VERSION#"v"}"
 VERSION_SPLIT=(${LATEST_VERSION//./ })
-MAJOR=${VERSION_SPLIT[0]}
-MINOR=${VERSION_SPLIT[1]}
-PATCH=${VERSION_SPLIT[2]}
+MAJOR=${VERSION_SPLIT[0]-0}
+MINOR=${VERSION_SPLIT[1]-0}
+PATCH=${VERSION_SPLIT[2]-0}
 
 echo "Current semver: $MAJOR.$MINOR.$PATCH"
-echo "Commit message: $LATEST_COMMIT_MSG"
+echo "Commit message:\n---\n$LATEST_COMMIT_MSG\n---\n"
 
-# detect if this was a PR merge, and what PR it was
+if [[ ! -z "$TRAVIS_TAG" ]]; then
+    echo "Current commit ($TRAVIS_COMMIT) already has a tag"
+    echo "Don't tag a PR merge commit manually, exiting"
+    exit 0
+fi
 
-# get PR body
+if [[ ! "$TRAVIS_BRANCH" =~ $DEPOY_BRANCH ]]; then
+    echo "Not on deploy branch, exiting"
+    exit 0
+fi
 
-# detect version change type from PR body
+RESULT_FILE="${TRAVIS_COMMIT}_pr_tag.json"
 
-# do version change
+if [[ -z "$GITHUB_OAUTH_TOKEN" ]]; then
+    curl https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls/${PR_NUM} | jq '[.body, .merged]' > $RESULT_FILE
+else
+    curl -H "Authorization: token ${GITHUB_OAUTH_TOKEN}" https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls/${PR_NUM} | jq '[.body, .merged]' > $RESULT_FILE
+fi
 
-# create new tag for this PR merge commit, push it
+if [[ -z "$(cat $RESULT_FILE)" ]]; then
+    echo "No result retrieved"
+    exit 0
+fi
 
-# set TRAVIS_TAG to new tag
+export PR_BODY=$(cat $RESULT_FILE | jq '.[0]')
+export MERGED=$(cat $RESULT_FILE | jq '.[1]')
 
-# set env var with PR body
+echo "PR_BODY: \n$PR_BODY"
 
+if [[ "$PR_BODY" =~ "${VERSION_CHANGE_PREFIX}${VERSION_CHANGE_TYPES[0]}" ]]; then
+    # patch version
+    PATCH=$((PATCH+1))
+elif [[ "$PR_BODY" =~ "${VERSION_CHANGE_PREFIX}${VERSION_CHANGE_TYPES[1]}" ]]; then
+    # minor version
+    PATCH="0"
+    MINOR=$((MINOR+1))
+elif [[ "$PR_BODY" =~ "${VERSION_CHANGE_PREFIX}${VERSION_CHANGE_TYPES[1]}" ]]; then
+    # major version
+    PATCH="0"
+    MINOR="0"
+    MAJOR=$((MAJOR+1))
+else
+    # non-versioned
+    echo "Detected non-versioned change, exiting"
+    exit 0
+fi
 
+if [[ "$MERGED" != "true" ]]; then
+    echo "PR is not merged, exiting"
+    exit 0
+fi
+
+export TRAVIS_TAG="v$MAJOR.$MINOR.$PATCH"
+
+# create tag
+git tag $TRAVIS_TAG
+git push --tags
+
+export DO_GITHUB_RELEASE="true"
 
